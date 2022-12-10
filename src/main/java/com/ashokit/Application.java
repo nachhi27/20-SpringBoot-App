15cf6c124a31c8896fd279c05ede5ad361a9d699
unknown <nachi27@gmail.com>
2022-12-10 17:25:30 +0530

    HIS-134 fix provided

--- src/main/java/com/ashokit/Application.java
+++ src/main/java/com/ashokit/Application.java
@@ -10,7 +10,10 @@ public class Application {
 	public static void main(String[] args) {
 		//SpringApplication.run(Application.class, args);
 		ConfigurableApplicationContext run = SpringApplication.run(Application.class);
-		run.close();
+		
+		//Changes for HIS-134 
+		run.close();		
+	    
 	}
 
 }
